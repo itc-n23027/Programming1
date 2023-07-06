9071595ccdd12255ae0152f7475e73fb79c228c8
Maeshiro Ren <n23027@std.it-college.ac.jp>
2023-07-06 06:23:56 +0000

    commit

---
+++ CHAPTER02/Q2_6_2.py
@@ -0,0 +1,9 @@
+title = "address"
+value = "tokyo"
+string = "{}:{}"
+print(string.format(title, value))
+
+subtitle = "Maeshiro"
+subvalue = "Ren"
+substring = "{} {}"
+print(substring.format(subtitle, subvalue))
