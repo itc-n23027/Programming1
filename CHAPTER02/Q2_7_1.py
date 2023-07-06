c854d6e7f7f9a25c096b7ba4d09e28bcffb7e349
Maeshiro Ren <n23027@std.it-college.ac.jp>
2023-07-06 07:22:02 +0000

    commit

---
+++ CHAPTER02/Q2_7_1.py
@@ -0,0 +1,17 @@
+mylist = []
+mylist.append("orange")
+mylist.append("apple")
+mylist.append("grape")
+mylist.append("banana")
+print(mylist)
+
+sublist = []
+sublist.append("M")
+sublist.append("a")
+sublist.append("e")
+sublist.append("s")
+sublist.append("h")
+sublist.append("i")
+sublist.append("r")
+sublist.append("o")
+print(sublist)
---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,15 @@
+my_list = ["orange", "apple", "grape", "banana"]
+my_list.sort()
+print(my_list)
+
+my_list = ["orange", "apple", "grape", "banana"]
+my_list.sort(reverse=True)
+print(my_list)
+
+sublist = [1, 3, 5, 7, 2, 4, 8]
+sublist.sort()
+print(sublist)
+
+sublist = [1, 3, 5, 7, 2, 4, 8]
+sublist.sort(reverse=True)
+print(sublist)
