44f8bd28a8b95b387064c73126ec13835b84cf44
Maeshiro Ren <n23027@std.it-college.ac.jp>
2023-07-12 07:35:54 +0000

    commit

--- CHAPTER03/Q3_3_1.py
+++ CHAPTER03/Q3_3_1.py
@@ -5,3 +5,5 @@ for s in my_list:
         my_list_6.append(s)
 print(my_list_6)  # 教科書
 
+my_list = ["tokyo", "osaka", "fukuoka", "aichi", "kyoto", "chiba", "saitama", "gunma"]
+print([i for i in my_list if len(i) >= 6])  # 短くした
--- CHAPTER03/Q3_3_2.py
+++ CHAPTER03/Q3_3_2.py
@@ -1,6 +1,10 @@
-my_list = [1, 1, 3, 4, 6, 9, 13]
+my_list = [1, 1, 2, 3, 5, 8, 13]
 x = 0
 for n in my_list:
     if n % 2 != 0:
         x += n
+print(x)  # 教科書
+
+my_list = [1, 1, 2, 3, 5, 8, 13]
+x = sum([i for i in my_list if i % 2 != 0])
 print(x)
